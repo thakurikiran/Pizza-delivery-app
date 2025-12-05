8014ef4321b1bca7ebe8770dbd8daeda8cb88cb6
kiran shahi <shahikiran575@gmail.com>
2025-12-05 11:18:27 +0545

    ubdate all folders

---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import {Link } from "react-router-dom"
+function Header(){
+    return (
+       <header>
+        <Link to="/">Fast React Pizza Go.</Link>
+
+        <p>Kiran</p>
+       </header>
+
+
+    )
+}
+
+export default Header;
---
+++ src/ui/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <h1>
+        The best pizza.
+        <br />
+        Straight out of the oven, straight to you.
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
+
+
